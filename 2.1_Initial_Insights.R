c922f118b002bddeec77745d80fd2315c22ef167
Glenn Aranha <glennaranha@yahoo.com>
2025-01-04 14:44:53 +0000

    Update 2.1_Initial_Insights.R

--- 2.1_Initial_Insights.R
+++ 2.1_Initial_Insights.R
@@ -1,13 +1,6 @@
 #Get an initial summary of the dataset
 summary(PLD)
 
-#Take the columns we want and filter for data with integrity
-PLD <- PLD %>%
-  filter(Tested == "Yes") %>%
-  filter(Sanctioned == "Yes") %>%
-  select(Name, Sex, Equipment, Age, BodyweightKg, Best3BenchKg, Place, 
-         Country, ParentFederation, Date, MeetCountry)
-
 #See how many unique individuals there are
 length(unique(PLD$Name))
 
@@ -20,3 +13,10 @@ table(PLD$Sanctioned)
 table(PLD$ParentFederation)
 table(PLD$Country)
 table(PLD$MeetCountry)
+
+#Take the columns we want and filter for data with integrity
+PLD <- PLD %>%
+  filter(Tested == "Yes") %>%
+  filter(Sanctioned == "Yes") %>%
+  select(Name, Sex, Equipment, Age, BodyweightKg, Best3BenchKg, Place, 
+         Country, ParentFederation, Date, MeetCountry)
